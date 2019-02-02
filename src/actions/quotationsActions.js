3e0efc910aad170aacc67999da061e818662b9aa
Sergii Doryba <golfersds@gmail.com>
2019-02-02 16:47:26 +0200

    change type request not secure HTTPS

--- src/actions/quotationsActions.js
+++ src/actions/quotationsActions.js
@@ -4,7 +4,7 @@ export function getQuotations(pageNumber=1) {
   const proxyurl = "https://cors-anywhere.herokuapp.com/";
   return (dispatch) => {
 
-    axios.get(`https://199.247.18.85/api/v1/quotations?page=${pageNumber}`)
+    axios.get(`http://199.247.18.85/api/v1/quotations?page=${pageNumber}`)
       .then(response => {
         dispatch({
           type: 'LOAD_QUOTATIONS_SUCCESS',
